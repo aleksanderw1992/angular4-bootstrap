239d72e1b0a12ba16f2ce76a44dc295bd4d4049c
Aleksander Wójcik <aleksander.w1992@wp.pl>
2017-05-13 21:21:59 +0200

    Images fix :)

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent {
 
-  constructor() { }
+  logoPath;
+  constructor() {
+    this.logoPath='\\assets\\img\\logocompany.png'
+  }
 
 
 }
