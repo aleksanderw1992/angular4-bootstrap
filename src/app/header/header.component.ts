223950b1a768e631ed60760db88a6c9df5580e0c
Aleksander Wójcik <aleksander.w1992@wp.pl>
2017-05-18 20:56:09 +0200

    Repair bootstrap. Do dropdownmenu

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
-
   logoPath='\\assets\\img\\logocompany.png'
+  dropdownLabels:Array<string> = [
+    'Strona główna',
+    'Zarządzanie elementami',
+    'Konfiguracja aplikacji'
+  ];
+  selectedPage:string= this.dropdownLabels[0];
   constructor() {
   }
 
-
+  onToogle(index){
+    this.selectedPage= this.dropdownLabels[index];
+  }
 }
