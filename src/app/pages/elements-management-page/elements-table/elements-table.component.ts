4ce73f6752efd811c42b8a410ecf91c54a5925c1
Aleksander Wójcik <aleksander.w1992@wp.pl>
2017-05-19 14:14:05 +0200

    Adding table model

--- src/app/pages/elements-management-page/elements-table/elements-table.component.ts
+++ src/app/pages/elements-management-page/elements-table/elements-table.component.ts
@@ -1,28 +1,35 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params, Router } from '@angular/router';
-import { AppEventHolder } from '../../../common/app-events-holder.service'
-
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Params, Router} from '@angular/router';
+import {AppEventHolder} from '../../../common/app-events-holder.service'
+import {TableModel} from './table-model.model';
 
 @Component({
   selector: 'app-elements-table',
   templateUrl: './elements-table.component.html',
-  styleUrls: ['./elements-table.component.scss']
+  styleUrls: ['./elements-table.component.scss'],
+  providers: [TableModel]
 })
 export class ElementsTableComponent implements OnInit {
-  element='';
+  element = '';
+  model: any;
+
+
   constructor(private router: Router,
               private route: ActivatedRoute,
-  private appEventHolder:AppEventHolder) {
+              private appEventHolder: AppEventHolder,
+              private tableModel: TableModel) {
   }
 
   ngOnInit() {
     this.route.params
-        .subscribe(
+      .subscribe(
         (params: Params) => {
           this.element = params['element-name'];
-            this.appEventHolder.publishElement(this.element)
+          this.appEventHolder.publishElement(this.element);
+          this.model = this.tableModel.model[this.element];
+          console.log(this.model);
         }
-    );
+      );
   }
 
 }
---
+++ src/app/pages/elements-management-page/elements-table/table-model.model.ts
@@ -0,0 +1,44 @@
+
+export class TableModel{
+  public model ={
+    author:{
+      columns:[
+        'Imie',
+        'Nazwisko',
+        'Kraj'
+      ]
+    },
+    category:{
+      columns:[
+        'Nazwa'
+      ]
+    },
+    country:{
+      columns:[
+        'Nazwa w j. oryginalnym',
+        'Flage'
+      ]
+    },
+    book:{
+      columns:[
+        'Tytuł',
+        'Autor',
+        'ISBN',
+        'Kategorie',
+        'Rodzaje nośników',
+        'Rodzaje okładek',
+        'Cena',
+        'Okładka'
+      ]
+    },
+    medium:{
+      columns:[
+        'Nazwa'
+      ]
+    },
+    cover:{
+      columns:[
+        'Nazwa']
+    },
+  }
+}
