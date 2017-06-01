6dc3fd7ff5880af091ef6001c322f1a162db9762
Aleksander Wójcik <aleksander.w1992@wp.pl>
2017-06-01 21:11:04 +0200

    small fix - extend directive

--- src/app/common/directives/dropdown.directive.ts
+++ src/app/common/directives/dropdown.directive.ts
@@ -1,16 +1,25 @@
-import { Directive, HostListener, HostBinding } from '@angular/core';
+import {Directive, HostListener, HostBinding, Input} from '@angular/core';
 
 @Directive({
   selector: '[appDropdown]'
 })
 export class DropdownDirective {
   @HostBinding('class.open') isOpen = false;
+  @Input() closeOnMouseLeave: string;
 
   @HostListener('click') toggleOpen() {
     this.isOpen = !this.isOpen;
   }
 
   @HostListener('mouseleave') mouseleave() {
-    this.isOpen = false;
+    /*
+    todo WTF?
+    @Input() closeOnMouseLeave: boolean;
+    it is still in runtime
+    typeof this.closeOnMouseLeave ==="string"
+     */
+    if(this.closeOnMouseLeave ==="true"){
+      this.isOpen = false;
+    }
   }
 }
